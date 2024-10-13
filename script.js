9a138ab9df722c03929a575b00b4feafde1a03a7
Fabricio <fa.engeroff1996@gmail.com>
2024-10-13 07:30:09 -0300

    criado o botão checkbox e editar.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+let contador = 0;
+
 
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
@@ -12,6 +14,24 @@ function adicionarItem(evento){
     containerItemLista.classList.add("lista-item-container");
 
     const conteinerNomeDoItem = document.createElement("div");
+
+    const containerCheckbox = document.createElement("div");
+    containerCheckbox.classList.add("container-checkbox");
+    const checkboxInput = document.createElement("input");
+    checkboxInput.type = "checkbox";
+    checkboxInput.classList.add("input-checkbox");
+    checkboxInput.id = "checkbox" + contador++;
+    const checkboxLabel = document.createElement("label");
+    checkboxInput.setAttribute("for", checkboxInput.id);
+
+    const checkboxCustomizado = document.createElement("div");
+    checkboxCustomizado.classList.add("checkbox-customizado");
+
+    checkboxLabel.appendChild(checkboxInput);
+    checkboxLabel.appendChild(checkboxCustomizado);
+    containerCheckbox.appendChild(checkboxLabel);
+    conteinerNomeDoItem.appendChild(containerCheckbox);
+
     const nomeDoItem = document.createElement("p");
     nomeDoItem.innerText = item.value;
     conteinerNomeDoItem.appendChild(nomeDoItem);
@@ -23,9 +43,16 @@ function adicionarItem(evento){
     const imagemRemover = document.createElement("img");
     imagemRemover.src = "img/delete.svg";
     imagemRemover.alt = "Remover";
-
     botaoRemover.appendChild(imagemRemover);
     conteinerBotoes.appendChild(botaoRemover);
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("item-lista-button");
+    const imagemEditar = document.createElement("img");
+    imagemEditar.src = "img/edit.svg";
+    imagemEditar.alt = "Editar";
+    botaoEditar.appendChild(imagemEditar);
+    conteinerBotoes.appendChild(botaoEditar);
 
     containerItemLista.append(conteinerNomeDoItem);
     containerItemLista.append(conteinerBotoes);
