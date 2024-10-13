f9cdefc2c69ee66f83e269813d8c08c05898c0d6
Fabricio <fa.engeroff1996@gmail.com>
2024-10-13 09:02:57 -0300

    Adicionada funcionalidade de inserção de item nas listas de compras ou desejos, com registro de data e horário.

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 const item = document.getElementById("input-item");
 const botaoSalvarItem = document.getElementById("adicionar-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
+const listaComprados = document.getElementById("lista-comprados");
 let contador = 0;
-
 
 botaoSalvarItem.addEventListener("click", adicionarItem);
 
-function adicionarItem(evento){
+function adicionarItem(evento) {
     evento.preventDefault();
-    
+
     const itemDaLista = document.createElement("li");
     const containerItemLista = document.createElement("div");
     containerItemLista.classList.add("lista-item-container");
 
-    const conteinerNomeDoItem = document.createElement("div");
+    const containerNomeDoItem = document.createElement("div");
 
     const containerCheckbox = document.createElement("div");
     containerCheckbox.classList.add("container-checkbox");
+
     const checkboxInput = document.createElement("input");
     checkboxInput.type = "checkbox";
     checkboxInput.classList.add("input-checkbox");
-    checkboxInput.id = "checkbox" + contador++;
+    checkboxInput.id = "checkbox-" + contador++;
+
     const checkboxLabel = document.createElement("label");
-    checkboxInput.setAttribute("for", checkboxInput.id);
+    checkboxLabel.setAttribute("for", checkboxInput.id);
+
+    checkboxLabel.addEventListener("click", function(evento){
+        const checkboxInput = evento.currentTarget.querySelector(".input-checkbox");
+        const checkboxCustomizado = evento.currentTarget.querySelector(".checkbox-customizado");
+        const itemTitulo = evento.currentTarget.closest("li").querySelector("#item-titulo");
+
+        if(checkboxInput.checked){
+            checkboxCustomizado.classList.add("checked");
+            itemTitulo.style.textDecoration = "line-through";
+            listaComprados.appendChild(itemDaLista);
+        } else{
+            checkboxCustomizado.classList.remove("checked");
+            itemTitulo.style.textDecoration = "none";
+            listaDeCompras.appendChild(itemDaLista);
+        }
+    })
 
     const checkboxCustomizado = document.createElement("div");
     checkboxCustomizado.classList.add("checkbox-customizado");
@@ -30,13 +48,14 @@ function adicionarItem(evento){
     checkboxLabel.appendChild(checkboxInput);
     checkboxLabel.appendChild(checkboxCustomizado);
     containerCheckbox.appendChild(checkboxLabel);
-    conteinerNomeDoItem.appendChild(containerCheckbox);
+    containerNomeDoItem.appendChild(containerCheckbox);
 
     const nomeDoItem = document.createElement("p");
+    nomeDoItem.id = "item-titulo";
     nomeDoItem.innerText = item.value;
-    conteinerNomeDoItem.appendChild(nomeDoItem);
+    containerNomeDoItem.appendChild(nomeDoItem);
 
-    const conteinerBotoes = document.createElement("div");
+    const containerBotoes = document.createElement("div");
     const botaoRemover = document.createElement("button");
     botaoRemover.classList.add("item-lista-button");
 
@@ -44,19 +63,26 @@ function adicionarItem(evento){
     imagemRemover.src = "img/delete.svg";
     imagemRemover.alt = "Remover";
     botaoRemover.appendChild(imagemRemover);
-    conteinerBotoes.appendChild(botaoRemover);
+    containerBotoes.appendChild(botaoRemover);
 
     const botaoEditar = document.createElement("button");
     botaoEditar.classList.add("item-lista-button");
+
     const imagemEditar = document.createElement("img");
     imagemEditar.src = "img/edit.svg";
     imagemEditar.alt = "Editar";
     botaoEditar.appendChild(imagemEditar);
-    conteinerBotoes.appendChild(botaoEditar);
+    containerBotoes.appendChild(botaoEditar);
 
-    containerItemLista.append(conteinerNomeDoItem);
-    containerItemLista.append(conteinerBotoes);
+    containerItemLista.appendChild(containerNomeDoItem);
+    containerItemLista.appendChild(containerBotoes);
+
+
+    const itemData = document.createElement("p");
+    itemData.innerText = `${new Date().toLocaleDateString("pt-BR", {weekday: "long"})} (${new Date().toLocaleDateString()}) às ${new Date().toLocaleTimeString("pt-BR", {hour:"numeric", minute:"numeric"})}`;
+    itemData.classList.add("texto-data");
+    
+    containerItemLista.appendChild(itemData);
     itemDaLista.appendChild(containerItemLista);
     listaDeCompras.appendChild(itemDaLista);
-   
 }
