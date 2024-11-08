04fe08fe48d3a34b7cc26b90d3a814a66229e780
Fabricio <fa.engeroff1996@gmail.com>
2024-11-08 08:43:13 -0300

    iniciei o projeto com criacao de um beckend node.js, api.js, sltyles.css e html.

--- js/adicionarItem.js
+++ js/adicionarItem.js
@@ -6,7 +6,7 @@ const item = document.getElementById("input-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
 
 export function adicionarItem(evento) {
-    evento.preventDefault()
+    evento.preventDefault();
 
     if (item.value === "") {
         alert("Por favor insira um item!");
