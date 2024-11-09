658d3ca2cea85c4ae7b171c34a200678d074bb51
Fabricio <fa.engeroff1996@gmail.com>
2024-11-09 07:17:45 -0300

    criado o metodo Salvar Pensamento Post.

---
+++ ui.js
@@ -0,0 +1,39 @@
+const ui = {
+    async renderizarPensamentos() {
+      const listaPensamentos = document.getElementById("lista-pensamentos")
+  
+      try {
+        const pensamentos = await api.buscarPensamentos()
+        pensamentos.forEach(pensamento => {
+          listaPensamentos.innerHTML += `
+            <li class="li-pensamento" data-id="${pensamento.id}">
+            <img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">
+            <div class="pensamento-conteudo">${pensamento.conteudo}</div>
+            <div class="pensamento-autoria">${pensamento.autoria}</div>
+            </li>
+          `
+        })
+      }
+      catch {
+        alert('Erro ao renderizar pensamentos')
+      }
+    }
+  }
+
+    salvarPensamento(pensamento) {
+      try {
+          const response = await fetch('http://localhost:3000/pensamentos', {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json"
+              },
+              body: JSON.stringify(pensamento)
+          })
+          return await response.json()
+      }
+      catch {
+          alert('Erro ao buscar pensamentos')
+          throw error
+      }
+  }
+  
