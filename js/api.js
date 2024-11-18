d7c8712ccaa200eb6de0f00b91a0a6219387dd22
Fabricio <fa.engeroff1996@gmail.com>
2024-11-18 07:48:32 -0300

    Codigo refatorado e atualizado.

--- js/api.js
+++ js/api.js
@@ -2,65 +2,65 @@ const URL_BASE = "http://localhost:3000";
 
 const converterStringParaData = (dataString) => {
   const [ano, mes, dia] = dataString.split("-");
-  return new Date(Date.UTC(ano, mes, dia));
+  return new Date(Date.UTC(ano, mes - 1, dia)); 
 }
 
 const api = {
   async buscarPensamentos() {
     try {
-      const response = await axios.get(`${URL_BASE}/pensamentos`)
-      const pensamentos = await response.data
+      const response = await axios.get(`${URL_BASE}/pensamentos`);
+      const pensamentos = response.data;
 
       return pensamentos.map(pensamento => {
         return {
           ...pensamento,
-          data: new Date(pensamento.data)
+          data: new Date(pensamento.dataHoraUTC)
         }
-      })
-    }
-    catch {
-      alert('Erro ao buscar pensamentos')
-      throw error
+      });
+    } catch (error) {
+      alert('Erro ao buscar pensamentos');
+      throw error;
     }
   },
 
   async salvarPensamento(pensamento) {
     try {
-      const data = converterStringParaData(pensamento.data)
+      const data = converterStringParaData(pensamento.data);
       const response = await axios.post(`${URL_BASE}/pensamentos`, {
         ...pensamento,
-        data: data.toISOString()
-      })
-      return await response.data
-    }
-    catch {
-      alert('Erro ao salvar pensamento')
-      throw error
+        dataHoraUTC: data.toISOString()
+      });
+      return response.data;
+    } catch (error) {
+      alert('Erro ao salvar pensamento');
+      throw error;
     }
   },
 
   async buscarPensamentoPorId(id) {
     try {
-      const response = await axios.get(`${URL_BASE}/pensamentos/${id}`)
-      const pensamento = await response.data
+      const response = await axios.get(`${URL_BASE}/pensamentos/${id}`);
+      const pensamento = response.data;
 
       return {
         ...pensamento,
-        data: new Date(pensamento.data)
+        data: new Date(pensamento.dataHoraUTC)
       }
-    }
-    catch {
-      alert('Erro ao buscar pensamento')
-      throw error
+    } catch (error) {
+      alert('Erro ao buscar pensamento');
+      throw error;
     }
   },
 
   async editarPensamento(pensamento) {
     try {
-      const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, pensamento);
-      return await response.data;
-    }
-    catch {
+      const data = converterStringParaData(pensamento.data);
+      const response = await axios.put(`${URL_BASE}/pensamentos/${pensamento.id}`, {
+        ...pensamento,
+        dataHoraUTC: data.toISOString()
+      });
+      return response.data;
+    } catch (error) {
       alert('Erro ao editar pensamento');
       throw error;
     }
@@ -68,9 +68,8 @@ const api = {
 
   async excluirPensamento(id) {
     try {
-      const response = await axios.delete(`${URL_BASE}/pensamentos/${id}`);
-    }
-    catch {
+      await axios.delete(`${URL_BASE}/pensamentos/${id}`);
+    } catch (error) {
       alert('Erro ao excluir um pensamento');
       throw error;
     }
@@ -81,14 +80,15 @@ const api = {
       const pensamentos = await this.buscarPensamentos();
       const termoEmMinusculo = termo.toLowerCase();
 
-      pensamentosFiltrados = pensamentos.filter(pensamento => {
-        return (pensamento.conetudo.toLowerCase().include(termoEmMinusculo)) ||
-          pensamento.aoutoria.toLowerCase().include(termoEmMinusculo);
-      })
+      const pensamentosFiltrados = pensamentos.filter(pensamento => {
+        return (
+          pensamento.conteudo.toLowerCase().includes(termoEmMinusculo) ||
+          pensamento.autoria.toLowerCase().includes(termoEmMinusculo)
+        );
+      });
       return pensamentosFiltrados;
 
-    }
-    catch (error) {
+    } catch (error) {
       alert("Erro ao filtrar pensamentos");
       throw error;
     }
@@ -96,14 +96,15 @@ const api = {
 
   async atualizarFavorito(id, favorito) {
     try {
-      const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, { favorito })
-      return response.data
+      const response = await axios.patch(`${URL_BASE}/pensamentos/${id}`, { favorito });
+      return response.data;
     } catch (error) {
-      alert("Erro ao atualizar favorito")
-      throw error
+      alert("Erro ao atualizar favorito");
+      throw error;
     }
   }
 }
 
 
 export default api;
+
